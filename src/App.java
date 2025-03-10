921617bfb55b667346a4edbfa068f13061638db0
Joshua Wangia <joshuawangia@gmail.com>
2025-03-10 09:16:04 -0400

    working on the fix

--- src/App.java
+++ src/App.java
@@ -5,18 +5,20 @@ public class App {
         Scanner myScanner = new Scanner(System.in);
         Guess testGuess = new Guess();
         QNA myQna = new QNA();
-
+        int nextQuestion = testGuess.getNextQuestion();
         while (testGuess.getLength() != 1) {
-            int nextQuestion = testGuess.getNextQuestion();
             System.out.println(myQna.askQuestion(nextQuestion));
 
             // String answers
             if (nextQuestion < 5 && nextQuestion % 3 != 0) {
                 testGuess.removeTeachers(myQna.Sanswer(nextQuestion, myScanner.nextLine()));
+                nextQuestion++;
             } 
             // Boolean Answers
             else {
                 testGuess.removeTeachers(myQna.Banswer(nextQuestion, Boolean.parseBoolean(myScanner.nextLine())));
+                nextQuestion++;
+
             }
         }
 
