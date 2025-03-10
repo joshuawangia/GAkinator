d2a80800cf6312702e05a1ceaecb3c70d3d789e6
Joshua Wangia <joshuawangia@gmail.com>
2025-03-10 10:38:49 -0400

    Error Is that the testGuess array is empty when initiated for some reason

--- src/App.java
+++ src/App.java
@@ -6,18 +6,21 @@ public class App {
         Guess testGuess = new Guess();
         QNA myQna = new QNA();
         int nextQuestion = testGuess.getNextQuestion();
-        while (testGuess.getLength() > 1) {
+        System.out.println(testGuess.Teachers);
+        while (testGuess.getLength() != 1) {
             System.out.println(myQna.askQuestion(nextQuestion));
 
             // String answers
             if (nextQuestion < 5 && nextQuestion % 3 != 0) {
                 testGuess.removeTeachers(myQna.Sanswer(nextQuestion, myScanner.nextLine()));
                 nextQuestion++;
+                System.out.println("removing " + nextQuestion);
             } 
             // Boolean Answers
             else {
                 testGuess.removeTeachers(myQna.Banswer(nextQuestion, Boolean.parseBoolean(myScanner.nextLine())));
                 nextQuestion++;
+                System.out.println("bremoving " + nextQuestion);
 
             }
         }
